f3d4fef0ca7f6cfd422e6e35e4ff1a83996ba9f4
Immaannn2222 <imennaayari@gmail.com>
2020-02-13 17:44:15 +0100

    fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - check the code for Holberton School students.
  *
@@ -10,22 +9,26 @@ int main(void)
 int i;
 for (i = 1 ; i <= 100 ; i++)
 {
-if (i % 3 == 0)
+  if (i % 3 == 0 && i % 5 != 0)
 {
-printf("Fizz ");
+printf("Fizz");
 }
-else if (i % 5 == 0)
+else if (i % 5 == 0 && i % 3 != 0)
 {
-printf("Buzz ");
+printf("Buzz");
 }
-if (i % 5 == 0 && i % 3 == 0)
+else if (i % 5 == 0 && i % 3 == 0)
 {
-printf("FizzBuzz ");
+printf("FizzBuzz");
 }
 else
 {
-printf("%d ", i);
+printf("%d", i);
 }
+ if (i != 100)
+   {
+     printf(" ");
+   }
 }
 printf("\n"); 
 return (0);
