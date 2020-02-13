8cb6662d5aa71185c4454528a19a9aabb92f1c4a
Immaannn2222 <imennaayari@gmail.com>
2020-02-13 21:29:14 +0100

    draw triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * print_triangle - prints a triangle
+ * @size: parameter 
+ * Return: void
+ *
+ */
+void print_triangle(int size)
+{
+int i;
+int j;
+int m;
+if (size > 0)
+{
+for (i = 0; i < size; i++)
+{
+for (j = size - i ; j > 1 ; j--)
+_putchar(' ');
+for (m = 0; m <= i ; m++)
+_putchar(35);
+_putchar('\n');
+}
+}
+else
+_putchar('\n');
+}
