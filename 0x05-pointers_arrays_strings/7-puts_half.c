6fec09eb2db1e0058070b7ad91ee0f88a3348190
Immaannn2222 <imennaayari@gmail.com>
2020-02-18 17:21:55 +0100

    add odd numbrs

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,6 +1,6 @@
 #include"holberton.h"
 /**
-*puts_half - prints a half of a string 
+*puts_half - prints a half of a string
 *@str : parameter character
 *Return:
 */
@@ -8,14 +8,22 @@ void puts_half(char *str)
 {
 int i, j;
 i = 0;
-j = i / 2;
+j = i  / 2;
 while (str[i] != '\0')
 {
 i++;
-} 
-for (j  = i/2 ; j <= i  ; j++)
+}
+if (i % 2 != 0)
+for (j  = i - 1 / 2 ; j <= i  ; j++)
 {
 _putchar(str[j]);
+}
+else
+{
+for (j = i / 2 ; j <= i ; j++)
+{
+_putchar(str[j]);
+}
 }
 _putchar('\n');
 }
