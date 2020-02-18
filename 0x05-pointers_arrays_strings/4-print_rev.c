28e1dd89a27c792debaaae4edceb0199dfec19d6
Immaannn2222 <imennaayari@gmail.com>
2020-02-18 15:05:48 +0100

    print in reverse take 3

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,13 +8,14 @@ void print_rev(char *s)
 {
 int i, j;
 i = 0;
+j = 0; 
 while (s[i] != '\0')
 {
 i++;
 }
-for (j = i ; j <= 0  ; j--)
+for (j = i - 1 ; j >= 0  ; j--)
 {
-_putchar(j);
+_putchar(s[j]);
 }
 _putchar('\n');
 }
