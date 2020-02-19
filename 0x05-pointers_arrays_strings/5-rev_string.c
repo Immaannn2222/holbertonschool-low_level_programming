ad31743ab779457f0b0cfe9908fdf7917d147876
Immaannn2222 <imennaayari@gmail.com>
2020-02-19 10:43:48 +0100

    space

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+
 /**
 *rev_string - prints a string in reverse
 *@s : parameter character
@@ -7,17 +8,13 @@
 
 void rev_string(char *s)
 {
-int i, j, x = 0;
+int i, j;
 i = 0;
 j = 0;
 char ch;
 while (s[i] != '\0')
 i++;
-if (i % 2 == 0)
-x = i / 2;   
-else
-x = (i /2) + 1;
-for (j = 0; j <= x; j++)
+for (j = 0; j <= i / 2; j++)
 {
 ch = s[j];
 s[j] = s[i - 1 - j];
