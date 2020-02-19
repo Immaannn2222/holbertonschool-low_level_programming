6b334e2bc8a58323577542c7cbfbd34b24b0be97
Immaannn2222 <imennaayari@gmail.com>
2020-02-19 10:24:16 +0100

    fic

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<math.h>
+
+/**
+ * getMaxPrimeFactor - returns the largest prime factor
+ * @n : parameter
+ * Return: Always 0.
+ */
+int getMaxPrimeFactor(int n)
+{
+int i, max = -1;
+while (n % 2 == 0)
+{
+max = 2;
+n = n / 2;
+}
+for (i = 3; i <= sqrt(n); i = i + 2)
+{
+while (n % i == 0)
+{
+max = i;
+n = n / i;
+}
+}
+if (n > 2)
+{
+max = n;
+}
+}
+main()
+{
+int n;
+printf("Enter a number: ");
+scanf("%d", &n);
+printf("Max prime factor: %d", getMaxPrimeFactor(n));
+}
+
---
+++ 0x05-pointers_arrays_strings/9-strpcy.c
@@ -0,0 +1,8 @@
+#include"holberton.h"
+/**
+* *_strpcy - copies a string eveb the null character
+* @src, @dest : parameter character
+*
+* Return:
+*/
+char *_strcpy(char *dest, char *src)
