82ebb1f75fdd8c39ce4c151ceaa30d8fe4dada64
Immaannn2222 <imennaayari@gmail.com>
2020-02-20 21:25:38 +0100

    fix all

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,9 +10,9 @@ char *cap_string(char *capi)
 int i;
 for (i = 0; capi[i] != '\0'; i++)
 {
-if (capi[i - 1] == 32 || capi[i - 1 ] == 46 || capi[i - 1] == 10 || capi [i - 1] == 10)
+if (capi[i - 1] == 32 || capi[i - 1] == 46 || capi[i - 1] == 10)
 {
-if (capi[i] >= 97 && capi[i] <= 122) 
+if (capi[i] >= 97 && capi[i] <= 122)
 capi[i] = capi[i] - 32;
 }
 }
