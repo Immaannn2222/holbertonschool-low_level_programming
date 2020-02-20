9894cf4d6f8e418040e3003de1ea8abfac580370
Immaannn2222 <imennaayari@gmail.com>
2020-02-20 20:13:20 +0100

    drive

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,8 +9,12 @@ char *cap_string(char *capi)
 {
 int i;
 for (i = 0; capi[i] != '\0'; i++)
-i++;
-if (capi[i] <= 32 && capi[i] >= 47)
-capi[i + 10] = capi[i + 10] - 32;
+{
+if (capi[i - 1] == 32 || capi[i - 1 ] == 46 || capi[i - 1] == 10 || capi [i - 1] == 10)
+{
+if (capi[i] >= 97 && capi[i] <= 122) 
+capi[i] = capi[i] - 32;
+}
+}
 return (capi);
 }
