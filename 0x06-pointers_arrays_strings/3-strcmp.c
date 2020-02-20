b749673aeabfcb2af13cc08d600ed056eeabcefb
Immaannn2222 <imennaayari@gmail.com>
2020-02-20 22:57:54 +0100

    break

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,9 +7,17 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-int i = 0, f = 0;
-while (i != '\0' && i != '\0' && s1[i] == s2[i])
+int i = 0, cre = 0;
+while (s1[i] != '\0' || s2[i] != '\0')
+{
+if (s1[i] != s2[i])
+{
+cre = s1[i] - s2[i];
+break;
+}
+else
+cre = 0;
 i++;
-f = (s1[i] - s2[i]);
-return (f);
+}
+return (cre);
 }
