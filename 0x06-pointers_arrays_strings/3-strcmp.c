1d3ec862e72a2fe19cb046f4dfed0be51df2a048
Immaannn2222 <imennaayari@gmail.com>
2020-02-20 12:22:57 +0100

    fix code

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,14 +7,15 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-if (s1[0] != s2[0])
+int i = 0, j = 0;
+if (s1[i] != s2[j])
 {
-return (s1[0] - s2[0]);
+return (s1[i] - s2[j]);
 }
-if (s1[0] == s2[0])
+if (s1[i] == s2[j])
 {
 return (0);
 }
 else
-return (s2[0] - s1[0]);
+return (s2[i] - s1[j]);
 }
