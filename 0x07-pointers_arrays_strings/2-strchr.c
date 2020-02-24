9a234de0b5beca3feadd67df98b0f19acf1ae80c
Immaannn2222 <imennaayari@gmail.com>
2020-02-24 11:44:04 +0100

    returns a character first position in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+*_strchr - Returns a pointer to the first occurrence of the character c in s
+*@s : a pointer
+*@c : a character
+*Return:0
+*/
+char *_strchr(char *s, char c)
+{
+int i = 0;
+for (i = 0 ; s[i] != '\0'; i++)
+{
+if (s[i] == c)
+{
+s = &s[i];
+return (s);
+}
+}
+return (0);
+}
