e779639addb1e6cc601eed8a7740477cfac2eb6a
Immaannn2222 <imennaayari@gmail.com>
2020-02-24 16:59:11 +0100

    floss

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,20 +8,15 @@
 char *_strpbrk(char *s, char *accept)
 {
 int i = 0, j = 0;
-for (i = 0; s[i] != '\0'; i++)
+for (i = 0; s[i] != '\0' ; i++)
 {
 for (j = 0; accept[j] != '\0'; j++)
 {
 if (s[i] == accept[j])
 {
-break;
+return (&s[i]);
 }
-++s;
-return (s);
 }
-if (accept[j] == '\0')
-break;
 }
 return (0);
 }
-
