e904392aeefb43cefe26c92d6400db706a723f3b
Immaannn2222 <imennaayari@gmail.com>
2020-02-24 10:54:27 +0100

    copies

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,17 +1,18 @@
 #include "holberton.h"
 /**
-*_memcpy - 
+* _memcpy - copies an address
 *@dest : parameter designing a character
 *@src : parametre designing a character
+*@n: an unsignd int
 *Return: dest
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i = 0;
-while ( i <= n)
+while (i <= n)
 {
 dest [i] = src[i];
 i++;
 }
 return (dest);
-} 
+}
