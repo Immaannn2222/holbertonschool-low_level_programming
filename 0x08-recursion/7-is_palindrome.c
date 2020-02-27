3636f9797b5114c3fde8a53979eab0133467ee82
Immaannn2222 <imennaayari@gmail.com>
2020-02-27 21:55:33 +0100

    palindrome

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,20 +1,46 @@
 #include "holberton.h"
 /**
-*is_palindrome - looks for palindrome
-*@s : parameter designing a character
-*Return: 1 if true 0 otherwise
-*/
-int is_palindrome(char *s)
+ * _strlen_recursion- returns the length of a string
+ * @s: parameter
+ *
+ * Return: len
+ */
+int _strlen_recursion(char *s)
 {
-char *result = 0;
 if (*s != '\0')
-{
-s++;
-is_palindrome(s--);
-*result = *s;
-}
-if (*result == *s)
-return (1);
+return (1 + _strlen_recursion(++s));
 else
 return (0);
+}
+
+/**
+ * pal - checks if its a palindrome
+ * @s: char
+ * @l: integer
+ *
+ * Return: pal
+ */
+int pal(char *s, int l)
+{
+if (l <= 1)
+return (1);
+else if (*s == s[l - 1])
+return (pal(s + 1, l - 2));
+else
+return (0);
+}
+
+/**
+ * is_palindrome - checks if palindrome
+ * @s: string
+ *
+ * Return: 1 or 0
+ */
+int is_palindrome(char *s)
+{
+int l;
+l = _strlen_recursion(s);
+if (l <= 1)
+return (1);
+return (pal(s, l));
 }
