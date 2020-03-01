5cf27aa423be408cdffe922858977e9f0536eaf6
Immaannn2222 <imennaayari@gmail.com>
2020-03-01 17:10:01 +0100

    fix file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+*main - a function tahta prints the name of a program
+*@argc : The number of command line arguments
+*@argv : An array containing the program command line arguments
+*
+*Return: 0
+*/
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+(void)argc;
+return (0);
+}
