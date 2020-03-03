ee1bc1d02232fbd8c18d2ecabcd9b9fd8df4cfa1
Immaannn2222 <imennaayari@gmail.com>
2020-03-03 14:32:13 +0100

    fae

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,9 +12,9 @@ char *create_array(unsigned int size, char c)
 unsigned int i;
 char *ar;
 ar =  malloc(size * sizeof(char));
-if (size == 0)
+if (size <= 0)
 {
-return (0);
+return (NULL);
 }
 for (i = 0 ; i < size; i++)
 {
