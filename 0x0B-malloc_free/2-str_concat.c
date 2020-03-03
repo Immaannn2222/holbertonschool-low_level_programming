c79002858252c785bb5af8dc82090f575ff9c1e9
Immaannn2222 <imennaayari@gmail.com>
2020-03-03 16:27:17 +0100

    try this one

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,22 +9,21 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-int i, j, m = 0;
+int i, j, m = 0, n = 0;
 char *s;
 if (s1 == NULL && s2 == NULL)
 return ("");
 while (s1[i] != '\0')
 i++;
 for (j = 0; s2[j] < '\0' ; j++)
+s = malloc((i + j + 1) * sizeof(char));
+for (m = 0; m <= i ; m++)
 {
+s[m] = s1[m];
 }
-s = malloc ((i + j + 1) * sizeof(char));
-while (s1[m] != '\0' && s2[m] <= '\0')
+for (n = i ; n <= j + i; n++)
 {
-s[m] = s1[m] + s2[m];
-m++;
+s[n] = s2[n];
 }
 return (s);
 }
-
- 
