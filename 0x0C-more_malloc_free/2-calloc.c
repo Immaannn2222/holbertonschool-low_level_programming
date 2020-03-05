d249420e6afac8f5f9a83cd4305424c759e9e5a0
Immaannn2222 <imennaayari@gmail.com>
2020-03-05 14:34:51 +0100

    fill the memory

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,9 +10,14 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 char *s;
+unsigned int i = 0;
 if ((nmemb == 0) || (size == 0))
 return (NULL);
 s = malloc(size * nmemb);
+for (i = 0; i <= nmemb; i++)
+{
+s[i] = 0;
+}
 if (s == NULL)
 return (NULL);
 return (s);
