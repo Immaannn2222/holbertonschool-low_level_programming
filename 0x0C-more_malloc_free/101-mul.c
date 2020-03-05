2aec9b7743b18ea8bf29e81e129c481cdb1555cb
Immaannn2222 <imennaayari@gmail.com>
2020-03-05 23:09:06 +0100

    out

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+*main - multipies 2 numbers
+*@argc :argument 1
+*@argv : argument 2
+*Return:mul
+*/
+int main(int argc, char *argv[])
+{
+char *num1, *num2, *mul;
+if (argc > 2)
+return ("Error");
+exit(98);
+if (argv[1, 2] < 48 || argv[1, 2] > 57)
+return ("Error");
+exit(98);
+return (mul = atoi(num1) * atoi(num2));
+
+
+}
