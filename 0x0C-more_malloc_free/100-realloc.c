b6cfe8807b99d98b349f6335b713668053ff2b57
Immaannn2222 <imennaayari@gmail.com>
2020-03-06 13:17:13 +0100

    fix again

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,12 +16,14 @@ ptr = malloc(new_size);
 return (ptr);
 }
 if (ptr == NULL)
+{
 ptr = malloc(new_size);
 return (ptr);
+}
 if (new_size == 0 && ptr != NULL)
 {
 free(ptr);
 return (NULL);
-}
 return (ptr);
+}
 }
