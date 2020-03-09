efca90036046cc9ffae8495607159f8212163b3c
Immaannn2222 <imennaayari@gmail.com>
2020-03-09 17:32:43 +0100

    buy a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,58 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*_strdup - returns a pointer to a newly allocated space in memory
+*@str : a char
+*
+*Return: null
+*/
+char *_strdup(char *str)
+{
+char *s;
+unsigned int i = 0, j = 0;
+if (str == NULL)
+return (NULL);
+while (str[i] != '\0')
+i++;
+s = malloc((i + 1) * sizeof(char));
+if (s == NULL)
+return (NULL);
+for (j = 0; j <= i; j++)
+{
+s[j] = str[j];
+}
+return (s);
+}
+
+/**
+*new_dog - a function that creates a new file
+*@name:its name
+*@age:its age
+*@owner:its owner's name
+*Return: newer
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *newer;
+newer = malloc(sizeof(struct dog));
+if (newer == NULL)
+{
+free(newer);
+return (NULL);
+}
+(*newer).name = _strdup(name);
+(*newer).owner = _strdup(owner);
+if ((*newer).name == NULL)
+{
+free(newer);
+return (NULL);
+}
+if ((*newer).owner == NULL)
+{
+free(newer);
+return (NULL);
+}
+(void)age;
+return (newer);
+}
