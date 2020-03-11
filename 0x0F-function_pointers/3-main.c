8191ad1454535e3e6639d83cbd4df566afd94eb4
Immaannn2222 <imennaayari@gmail.com>
2020-03-11 22:45:05 +0100

    not finished yet

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,6 @@
+#include"3-calc.h"
+/**
+*
+*
+*/
+void (*arr[])(num1, int) = {add, subtract, multiply, divise, modulo}; 
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+#include"3-calc.h"
+#include <stdlib.h>
+/**
+*op_add - a fuction to sum up 2 integers
+*@a : an integer
+*@b : an integer
+*Return: result
+*/
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+*op_sub - function for substitution
+*@a : an integer
+*@b : an intege
+*Return: resu
+*/
+int op_sub(int a, int b)
+{
+if (a > b)
+return (a - b);
+else
+(-(a - b));
+}
+
+/**
+*op_mul - a function for multiplication
+*@a : an integer
+*@b : an intege
+*Return: ress
+*/
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+*op_div - a function for division
+*@a : an integer
+*@b : an intege
+*Return: res
+*/
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+*op_mod - a function to calculate the rest in a division
+*@a : an integer
+*@b : an intege
+*Return: resd
+*/
+int op_mod(int a, int b);
+{
+return (a % b);
+}
