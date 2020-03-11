835d2cb124f200238d8e04446f507869c83ffd51
Immaannn2222 <imennaayari@gmail.com>
2020-03-11 20:37:30 +0100

    try this woth a type not nknown

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+/**
+*array_iterator - executes a function given as a parameter on each element
+*@array : the array to operate on
+*@size : its size
+*@action :the function
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+size_t i;
+if (size == 0 || array == NULL || action == NULL)
+return;
+for (i = 0; i < size; i++)
+action(array[i]);
+}
