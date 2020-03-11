8a8839ff65f109335fa01420968d16c48814ab6a
Immaannn2222 <imennaayari@gmail.com>
2020-03-11 23:53:42 +0100

    verify

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints the opcodes
+ * @argc: number arguments
+ * @argv: double pointer of arguments
+ * Return: 1 or 0 or 2
+ */
+int main(int argc, char *argv[])
+{
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
+return (0);
+}
