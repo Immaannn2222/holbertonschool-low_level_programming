cd1d1631ea31b3f066d496a546d1e3fcf5b4b698
Immaannn2222 <imennaayari@gmail.com>
2020-03-11 23:07:58 +0100

    almost there

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,25 @@
 #include"3-calc.h"
 /**
-*
-*
+*main - the main function that will operate
+*@argc : number of arguments
+*@argv: pointer to the arguments passed to the program
+*Return: a result
 */
-void (*arr[])(num1, int) = {add, subtract, multiply, divise, modulo}; 
+int main(int argc, char *argv[])
+{
+int x, y, result = 0;
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+if (argv[2][1])
+{
+printf("Error\n");
+exit(99);
+}
+x = atoi(argv[1]);
+y = atoi(argv[3]);
+result = (get_op_func(argv[2])(x, y);
+return (result);
+}
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -44,6 +44,11 @@ return (a * b);
 */
 int op_div(int a, int b)
 {
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
 return (a / b);
 }
 
@@ -55,5 +60,10 @@ return (a / b);
 */
 int op_mod(int a, int b);
 {
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
 return (a % b);
 }
