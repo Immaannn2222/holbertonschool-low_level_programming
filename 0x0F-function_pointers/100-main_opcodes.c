a0c477d6a8db7917e52e2977a8dd6e4406a096a4
Immaannn2222 <imennaayari@gmail.com>
2020-03-12 21:20:58 +0100

    compile

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,5 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
+
+/**
+ * fun - checks a function
+ * @s: the char to check
+ * @x: the integer to check
+ * Return: 0
+ */
+void fun(char *s, int x)
+{
+int i;
+for (i = 0 ; i < x ; i++)
+{
+printf("%02x", s[i] & 0xff);
+if (i != x - 1)
+printf(" ");
+}
+printf("\n");
+}
+
 /**
  * main - prints the opcodes
  * @argc: number arguments
@@ -13,5 +32,11 @@ if (argc != 2)
 printf("Error\n");
 exit(1);
 }
+if (atoi(argv[1]) < 0)
+{
+printf("Error\n");
+exit(2);
+}
+fun((char *)&main, atoi(argv[1]));
 return (0);
 }
