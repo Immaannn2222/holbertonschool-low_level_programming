23e28f56bed8112a2c0ee68f81ee68397808beff
Immaannn2222 <imennaayari@gmail.com>
2020-03-21 20:04:45 +0100

    fix new line

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,8 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
-#include <string.h>
 /**
  * print_list - a program to print list using lists
  * @h: a struct type pointer
@@ -17,9 +15,9 @@ size_t n = 0;
 while (h != NULL)
 {
 if (h->str == NULL)
-printf("[0] (nil) \n");
+printf("[0] (nil)\n");
 else
-printf("[%u] %s \n", h->len, h->str);
+printf("[%u] %s\n", h->len, h->str);
 h = h->next;
 n++;
 }
