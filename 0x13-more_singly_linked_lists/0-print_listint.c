6f4e17a0e65023e0d54c0e5d89b976b43dbb5169
Immaannn2222 <imennaayari@gmail.com>
2020-03-24 10:56:09 +0100

    verify1

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,10 +1,11 @@
 #include "lists.h"
 /**
-* print_listint - prints a list using a pre-defined structure
-* @h: parameter pointing to the nodes of list
-*
-* Return: number of nodes
-*/
+ * print_listint - prints a list using a pre-defined structure
+ * @h: parameter pointing to the nodes of list
+ *
+ * Return: number of nodes
+ */
+
 size_t print_listint(const listint_t *h)
 {
 size_t n = 0;
