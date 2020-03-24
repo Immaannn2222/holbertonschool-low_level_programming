0bd600c66d150a9b0ec3ed712c70399eb79b5422
Immaannn2222 <imennaayari@gmail.com>
2020-03-24 13:37:59 +0100

    check

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a list
+ * @head: double pointer to the list
+ *
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+listint_t **trav = NULL;
+while (*head != NULL)
+{
+*trav = (*head)->next;
+free(*head);
+head = trav;
+}
+}
