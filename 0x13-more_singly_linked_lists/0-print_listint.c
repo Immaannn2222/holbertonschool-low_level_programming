89fc5063ce75dd13774dee9194e391647f665fd2
Immaannn2222 <imennaayari@gmail.com>
2020-03-24 10:42:55 +0100

    fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 /**
- * print_listint - prints a list
- * @h: parameter
- *
- * Return: number of nodes
- */
+* print_listint - prints a list
+* @h: parameter
+*
+* Return: number of nodes
+*/
 size_t print_listint(const listint_t *h)
 {
-unsigned int n;
+size_t n;
 while (h != NULL)
 {
 if (n == '0')
