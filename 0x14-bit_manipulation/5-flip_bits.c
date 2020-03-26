8e1446641620eb2726913890ee0e0635fa922dba
Immaannn2222 <imennaayari@gmail.com>
2020-03-26 22:44:58 +0100

    fix

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * count - counts the sets
+ * @x: its operator
+ *
+ * Return: x
+ */
+
+unsigned long int count(unsigned long int x)
+{
+unsigned long int c = 0;
+while (x)
+{
+c += x & 1;
+x >>= 1;
+}
+return (x);
+}
+/**
+ * flip_bits - returns number of flips needed to transform n to m
+ *@n: number1
+ *@m: number2
+ *
+ * Return: result
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+return (count(n ^ m));
+}
