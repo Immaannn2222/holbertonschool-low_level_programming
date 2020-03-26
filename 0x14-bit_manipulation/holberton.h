4198e1bbe1ee2fefa3346e556b65c5eb09e2772e
Immaannn2222 <imennaayari@gmail.com>
2020-03-26 19:30:42 +0100

    add putchar

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -2,9 +2,9 @@
 #define BIT_MAN
 #include <stdlib.h>
 #include <stdio.h>
-#include <math.h>
 
 unsigned int binary_to_uint(const char *b);
+int _putchar(char c);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
