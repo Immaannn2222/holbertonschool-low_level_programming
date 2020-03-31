6491adbab64451a11c8f36d60eaa84e364c7f863
Immaannn2222 <imennaayari@gmail.com>
2020-03-31 22:17:59 +0100

    fix

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,16 +8,14 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-int fd, n;
+int fd, n, i;
 if (!filename)
 return (-1);
 fd = open(filename, O_WRONLY | O_APPEND);
 if (fd == -1)
 return (-1);
-if (!text_content && fd == 1)
+if (!text_content)
 return (1);
-else
-return (-1);
 for (i = 0; text_content[i] != '\0'; i++)
 {
 ;
