5499849c823f4c9c2e5875b6ec5669e97f1b1d85
Immaannn2222 <imennaayari@gmail.com>
2020-03-31 22:59:27 +0100

    try

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+/**
+ * main - copies the content of a file to another
+ * @argc: number of arguments
+ * @argv: content of the argumens
+ *
+ * Return: 0
+ */
+
+
+int main (int argc, char *argv[])
+{
+int file_to, file_from;
+char buffer[1024];
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+}
+file_from = open(argv[1], O_RDONLY);
+if (file_from == -1)
+{
+dprintf(STDERR_FILENO,"Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+file_from = read(file_from, buffer, 1024)
+file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+if (file_to == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+exit(99);
+}
+file_to =  write(file_from, buffer, 1024);
+if (file_to == -1)
+{
+dprintf(" Error: Can't write to %s\n", argv[2]);
+exit(99);
+}
+return (0);
+}
