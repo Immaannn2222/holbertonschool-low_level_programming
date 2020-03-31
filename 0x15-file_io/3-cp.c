7d7d6ddfb2f43e2825d59a33d7a4057d185667a0
Immaannn2222 <imennaayari@gmail.com>
2020-03-31 23:04:29 +0100

    fi

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
  * main - copies the content of a file to another
  * @argc: number of arguments
@@ -11,7 +10,7 @@
 
 int main (int argc, char *argv[])
 {
-int file_to, file_from;
+int file_to = 0, file_from = 0;
 char buffer[1024];
 if (argc != 3)
 {
@@ -24,7 +23,6 @@ if (file_from == -1)
 dprintf(STDERR_FILENO,"Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
-file_from = read(file_from, buffer, 1024)
 file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 if (file_to == -1)
 {
