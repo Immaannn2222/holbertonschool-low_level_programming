cec603285bc1bc06574727c706f7f4099d34ad7d
Immaannn2222 <imennaayari@gmail.com>
2020-04-01 12:15:37 +0100

    d

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,7 +24,7 @@ if (r == -1)
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 w = write(file_to, buffer, r);
 if (w == -1)
-dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]), exit(98);
+dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]), exit(99);
 }
 m = close(file_to);
 if (m == -1)
