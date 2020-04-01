4a5819fda6f8deb91a31130450fec0fe8d85d2ea
Immaannn2222 <imennaayari@gmail.com>
2020-04-01 11:59:36 +0100

    fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,30 +3,45 @@
  * main - copies the content of a file to another
  * @argc: number of arguments
  * @argv: content of the argumens
- *
  * Return: 0
  */
-
-
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-int file_to, file_from;
+int file_to, file_from, m, n, r, w;
+char buffer[1024];
 if (argc != 3)
-{
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-exit(97);
-}
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 file_from = open(argv[1], O_RDONLY);
 if (file_from == -1)
-{
-dprintf(STDERR_FILENO,"Error: Can't read from file %s\n", argv[1]);
-exit(98);
-}
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 if (file_to == -1)
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+m = close(file_to);
+n = close(file_from);
+if (m == -1)
+dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", argv[2]), exit(100);
+if (n == -1)
+dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", argv[1]), exit(100);
+while (r == 1024)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-exit(99);
+r = read(file_from, buffer, 1024);
+if (r == -1)
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+w = write(file_to, buffer, r);
+if (w == -1)
+dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]), exit(98);
 }
 return (0);
 }
+
+
+
+
+
+
+
+
+
+
+
