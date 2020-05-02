9e6bdc41c71786ab4eabfc62e11ebdde727df2b0
Immaannn2222 <imennaayari@gmail.com>
2020-05-02 13:01:42 +0100

    check 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,12 +1,19 @@
 #include "lists.h"
 /**
- * print_dlintint - prints all elements of a double linked list(DLL)
- * @h: a pointer to the list
- * 
- * Return: numbe of nodes 
+ * print_dlistint - prints all elements of a double linked list(DLL)
+ * @h: a pointer to the nodes of the lsit
+ *
+ * Return: numbe of nodes
  */
 
 size_t print_dlistint(const dlistint_t *h)
 {
-
+size_t x = 0;
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+x++;
+}
+return (x);
 }
