fe81dc090243f29ad145f2fa122a6bf08fe02c14
Immaannn2222 <imennaayari@gmail.com>
2020-05-02 13:44:45 +0100

    fix

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,8 +12,12 @@ dlistint_t *new;
 new = malloc(sizeof(dlistint_t));
 if (new == NULL)
 return (NULL);
+
 new->n = n;
 new->next = (*head);
+new->prev = NULL;
+if ((*head) != NULL)
+(*head)->prev = new;
 (*head) = new;
 return (new);
 }
