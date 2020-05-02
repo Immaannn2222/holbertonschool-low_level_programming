c27c4bcf11bb26535fd26319cd2407adecad1c55
Immaannn2222 <imennaayari@gmail.com>
2020-05-02 13:09:26 +0100

    check 1:1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,13 +2,15 @@
 /**
  * dlistint_len - returns the number of elements in a linked dlistint_t list
  * @h: a pointer to the nodes of the list
- * 
- * Return: length of tle DLL
+ *
+ * Return: length of the DLL
  */
 
 size_t dlistint_len(const dlistint_t *h)
 {
 size_t x;
+if (!h)
+return (0);
 while (h != NULL)
 {
 h = h->next;
