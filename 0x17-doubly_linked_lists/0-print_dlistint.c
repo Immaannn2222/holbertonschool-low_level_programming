10f5c55cf45747af331f26a4a0bcb3b87ac7c8ab
Immaannn2222 <imennaayari@gmail.com>
2020-05-02 02:46:54 +0100

    files present

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * print_dlintint - prints all elements of a double linked list(DLL)
+ * @h: a pointer to the list
+ * 
+ * Return: numbe of nodes 
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+
+}
