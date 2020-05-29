724c1beadeafb70bc6f3a3ac2d9f737a866c2f2c
Immaannn2222 <imennaayari@gmail.com>
2020-05-29 22:16:18 +0100

    corr

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 hash_node_t *new;
 unsigned long int idx = 0;
 idx = key_index((const unsigned char *)key, ht->size);
-if (ht == NULL || key == NULL)
+if (ht == NULL || key == NULL || value == NULL)
 {
 return (0);
 }
@@ -29,7 +29,14 @@ if (new == NULL)
 return (0);
 new->key = strdup(key);
 new->value = strdup(value);
+if (ht->array[idx] == NULL)
+{
+new->next = NULL;
+}
+else
+{
 new->next = ht->array[idx];
+}
 ht->array[idx] = new;
 return (1);
 }
