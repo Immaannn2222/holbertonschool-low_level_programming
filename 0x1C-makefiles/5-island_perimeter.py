a1b4baf66b63293b32378b674b151a1eded6c47e
Immaannn2222 <imennaayari@gmail.com>
2020-07-03 22:33:10 +0100

    fr

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,3 +4,9 @@
 
 def island_perimeter(grid):
     """ returns the perimeter of the island described in grid """
+    if grid is not None:
+        new = 0
+        for x in grid:
+            if x == 1:
+                new += 2
+        return new
